ec89347e479407c0e5483a3e12c5c2153b8eb030
Site <orejanatheo@gmail.com>
2026-08-18 15:22:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function mostrarMensagem() {
 
     alert(
-        "Olá! 👋\n\n" +
+        "Olá!\n\n" +
         "Obrigado pelo interesse nos eventos da Ma Maison Café.\n\n" +
         "Em breve você poderá entrar em contato diretamente pelo WhatsApp."
     );
