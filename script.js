d20c2cd32cb49bd2629ba290d00a91fa8b28ae0f
Site <orejanatheo@gmail.com>
2026-08-18 14:32:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarMensagem() {
+
+    alert(
+        "Olá! 👋\n\n" +
+        "Obrigado pelo interesse nos eventos da Ma Maison Café.\n\n" +
+        "Em breve você poderá entrar em contato diretamente pelo WhatsApp."
+    );
+
+}
